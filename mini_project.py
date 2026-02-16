ee69b652bd5c55ec6bdcec645004402de9681000
qqreyn <duch.jan.8@gmail.com>
2026-02-16 15:53:31 +0100

    Add files via upload

---
+++ mini_project.py
@@ -0,0 +1,13 @@
+name = "Janek"
+age = "Duch"
+city = "Warsaw"
+favorite_number = 2
+is_learning_python = True
+
+print(
+    "Name:", name, "\n" +
+    "Age:", age, "\n" +
+    "City:", city, "\n" +
+    "Favorite number:", favorite_number, "\n" +
+    "Is learning python:", is_learning_python
+)
